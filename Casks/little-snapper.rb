2990726d0324a78f47e7d026458fc1eff709e881
Pablo <pablo.ivulic@pablo.local>
2014-03-26 14:55:54 +0100

    Added LittleSnapper.app

---
+++ Casks/little-snapper.rb
@@ -0,0 +1,7 @@
+class LittleSnapper < Cask
+  url 'https://www.macupdate.com/download/29991/littlesnapper_185.zip'
+  homepage 'https://www.macupdate.com/'
+  version 'latest'
+  no_checksum
+  link 'LittleSnapper.app'
+end
